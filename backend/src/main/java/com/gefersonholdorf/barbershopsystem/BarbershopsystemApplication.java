0ca41fa7522afb9270beeb56da8ac9b97b41aea2
Geferson Holdorf <gefersonholdorf88@gmail.com>
2023-11-28 16:26:53 -0300

    Project created

---
+++ backend/src/main/java/com/gefersonholdorf/barbershopsystem/BarbershopsystemApplication.java
@@ -0,0 +1,13 @@
+package com.gefersonholdorf.barbershopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gefersonholdorf/barbershopsystem/BarbershopsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gefersonholdorf.barbershopsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
